2ce4d47275022d1aef9b3bdae95e769fe7c61255
Janice <janiceralecha@gmail.com>
2022-10-30 16:31:14 -0500

    commit tree ad52f8b153d6bafa473f6bbc7d72ee6a789654aa

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
+seconds_to_midnight = 5
+
+def countdown(seconds_to_midnight)
+    while seconds_to_midnight > 0
+        puts "#{seconds_to_midnight} SECOND(S)!"
+        seconds_to_midnight -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+# countdown(seconds_to_midnight)
+
+def countdown_with_sleep(seconds_to_midnight)
+    while seconds_to_midnight > 0
+        puts "#{seconds_to_midnight} SECOND(S)!"
+        sleep(1)
+        seconds_to_midnight -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+puts countdown(seconds_to_midnight)
+puts ""
+puts countdown_with_sleep(seconds_to_midnight)
